c26a9ceeafa882c714828e49a6de7e92958aa7f4
onaboat <140932608+onaboat@users.noreply.github.com>
2024-12-30 12:20:20 +1100

    particles (#1)

---
+++ app/components/particlebg.tsx
@@ -0,0 +1,117 @@
+"use client"
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+interface ParticleBgProps {
+  className?: string;
+  containerId?: string;
+}
+
+export const ParticleBg = ({ className = "", containerId = "tsparticles" }: ParticleBgProps) => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fullScreen: {
+        enable: false,
+        zIndex: 0
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#00DC7D",
+        },
+        links: {
+            distance: 125,
+            enable: true,
+            triangles: {
+                enable: true,
+                opacity: 0.05,
+            },
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 200,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <div className={`w-full h-full ${className}`}>
+        <Particles
+          id={containerId}
+          particlesLoaded={particlesLoaded}
+          options={options}
+          className="w-full h-full"
+        />
+      </div>
+    );
+  }
+
+  return <div className={`w-full h-full ${className}`} />;
+};
