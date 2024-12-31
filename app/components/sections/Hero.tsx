7152b1b54421cb0a9e22f31f3de517aa90365133
onaboat <onaboatatsea99@gmail.com>
2024-12-31 18:11:17 +1100

    telegram link

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -16,7 +16,6 @@ export default function Hero() {
                         <div className="max-w-lg mx-auto">
                             <p className="py-6 text-secondary">Simplify Token Management with Cutting-Edge zkCompression Technology—Fast, Secure, and Cost-Effective</p>
                         </div>
-                        <br></br>
                         <div className="max-w-lg mx-auto">
                             <p className="py-6 text-secondary hover:underline"><a href="https://t.me/zeaknavigatorgroup" className="text-primary">Join the revolution in token management. </a></p>
                         </div>
